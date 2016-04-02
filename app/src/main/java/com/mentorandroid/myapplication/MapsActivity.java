dbd825bdac18cba0e6967c5a1c15be95a9a712fb
BrunoHauck <brunohauck@gmail.com>
2016-04-02 11:15:49 -0300

    UP

--- app/src/main/java/com/mentorandroid/myapplication/MapsActivity.java
+++ app/src/main/java/com/mentorandroid/myapplication/MapsActivity.java
@@ -1,18 +1,24 @@
 package com.mentorandroid.myapplication;
 
-import android.support.v4.app.FragmentActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+
+    private List<MapItem> mapItemList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +30,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
+
+    public void addMarker(GoogleMap map, double lat, double lng, String title, String snippet){
+        map.addMarker(new MarkerOptions()
+                .position(new LatLng(lat, lng))
+                .title(title)
+                .snippet(snippet));
+    }
 
     /**
      * Manipulates the map once available.
@@ -38,9 +51,33 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        Intent i = getIntent();
+        mapItemList = (List<MapItem>)i.getSerializableExtra("object");
+        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+        int cont = 0;
+
+        for(MapItem mapItem : mapItemList) {
+
+            LatLng bounder = new LatLng(mapItem.getGeometry().getLat(), mapItem.getGeometry().getLng());
+            builder.include(bounder);
+            String coordinates = "Latitude: " + mapItem.getGeometry().getLat().toString() + "  Logitude: " + mapItem.getGeometry().getLng().toString();
+            addMarker(mMap, mapItem.getGeometry().getLat(), mapItem.getGeometry().getLng(), mapItem.getFormatted_address().toString(), coordinates);
+
+            final LatLngBounds bounds = builder.build();
+            mMap.setOnMapLoadedCallback(new GoogleMap.OnMapLoadedCallback() {
+                @Override
+                public void onMapLoaded() {
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, 10));
+                }
+            });
+
+        }
+
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //LatLng sydney = new LatLng(-34, 151);
+        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
+
+
 }
